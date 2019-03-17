ae920d12d91a6804004a31112c7b4b0a4271e8b8
GaelSantamaria <48625713+GaelSantamaria@users.noreply.github.com>
2019-03-17 20:20:01 +0100

    TutorialLayout and Playermovement

---
+++ FightingGame/Assets/Script/Bridge1Script.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge1Script : MonoBehaviour
+{
+    public Animator m_Animator;
+    private bool intractable;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (intractable == true && Input.GetKeyDown("e"))
+        {
+            m_Animator.SetTrigger("ButtonPressed");
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player"){
+            intractable = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player"){
+            intractable = false;
+        }
+    }
+}
