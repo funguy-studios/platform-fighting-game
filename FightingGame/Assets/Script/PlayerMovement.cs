6631d98f05bf7263868fbf04974dff47a4815e8b
Ryhime <ryhime100111@gmail.com>
2019-03-17 21:53:41 -0400

    Imporved Movement/Camera Overload

--- FightingGame/Assets/Script/PlayerMovement.cs
+++ FightingGame/Assets/Script/PlayerMovement.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Get ALL THE VARIABLES
     public float velocity = 5;
     public float jumpHeight = 5;
     public bool groundCheck;
     Vector2 input;
     private Rigidbody2D rb;
+    Vector2 movement = new Vector2(0, 0);
 
     void Start()
     {
+        //Get RB
         rb = GetComponent<Rigidbody2D>();
+        //Set the movement vector to current velocity (for gravity)
+        movement.y = rb.velocity.y;
     }
 
 
     void Update()
     {
+        //ALL THE FUNCTIONS
         GetInput();
         Jump();
-
+        Move();
         if (input.magnitude > 0.5f)
         {
             input.Normalize();
@@ -29,17 +35,21 @@ public class PlayerMovement : MonoBehaviour
         {
             return;
         }
-        Move();
+        //Actually Move with the vector
+        rb.velocity = movement;
     }
-    void GetInput()
+
+    public void GetInput()
     {
+        //Get input
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
     }
-    void Move()
-    {
-        float horizontalMovement = Input.GetAxis("Horizontal") * velocity;
 
+    public void Move()
+    {
+        float horizontalMovement = (Input.GetAxis("Horizontal") * velocity)*20;
+        //Flip Sprite
         if (horizontalMovement < 0)
         {
             GetComponent<SpriteRenderer>().flipX = true;
@@ -48,23 +58,26 @@ public class PlayerMovement : MonoBehaviour
         {
             GetComponent<SpriteRenderer>().flipX = false;
         }
+        //Frames for bad computers
         horizontalMovement *= Time.deltaTime;
-        transform.Translate(horizontalMovement, 0, 0);
+        //Set movement x
+        movement.x = horizontalMovement;
     }
-    void Jump()
+
+    public void Jump()
     {
+        //Check for able to jump
         if (Input.GetButtonDown("Jump") && groundCheck)
         {
-
-            rb.velocity = new Vector3(0, jumpHeight);
+            //Jumping
+            movement.y = jumpHeight+rb.velocity.y;
             groundCheck = false;
-            {
-
-            }
         }
+        else { movement.y = rb.velocity.y; }
     }
     void OnCollisionEnter2D(Collision2D other)
     {
+        //Change groundCheck variable
         if (other.gameObject.tag == "Ground")
         {
             groundCheck = true;
