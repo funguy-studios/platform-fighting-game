2434badd9114bbf7eb21ddaa8631701f824a9492
Ryhime <ryhime100111@gmail.com>
2019-03-19 18:42:23 -0400

    E

---
+++ FightingGame/Assets/Script/Enemies/Enemy_Basic.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Basic : MonoBehaviour
+{
+    public Transform enemy;
+    int move = 0;
+    const int movespeed=6;
+    int direction = 1; //1 == right -1 == left
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move++;
+        if (move >= 200) { direction = direction * -1; }
+        enemy.Translate(movespeed * direction, 0, 0);
+    }
+    //Kill Player(Will Be updated in the future when health system is in)
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
