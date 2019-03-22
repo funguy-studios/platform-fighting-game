89015b2170ffb345ba671cd8b2a979541ba93a1e
Charlie <34198241+Doomrider122@users.noreply.github.com>
2019-03-22 19:49:45 +0000

    Add files via upload

---
+++ FightingGame/Assets/Script/Elevator1Script.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator1Script : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed;
+    private bool canMove = false;
+    public int waitTime;
+    public Animator anim;
+    public GameObject lift;
+    public GameObject player;
+
+    private void Start()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canMove = true;
+            player.transform.parent = lift.transform;
+
+            if (Input.GetKey("e") && canMove == true)
+            {
+                anim.SetBool("LiftUp", true);
+                GetComponent<Animator>().enabled = true;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        canMove = false;
+        player.transform.parent = null;
+
+        if (canMove == false)
+        {
+            StartCoroutine(BackToStart());
+        }
+    }
+
+    IEnumerator BackToStart()
+    {
+        yield return new WaitForSeconds(waitTime);
+        anim.SetBool("LiftUp", false);
+        GetComponent<Animator>().enabled = true;
+    }
+}
