2ebc143ae606b38e660eae8ddb1e5b5a37f5b635
Ryhime <ryhime100111@gmail.com>
2019-03-24 13:32:50 -0400

    BIG Boy Enemy
    Added a bigger enemy and improved the basic enemy a tiny bit.

--- FightingGame/Assets/Script/Enemies/Enemy_Basic.cs
+++ FightingGame/Assets/Script/Enemies/Enemy_Basic.cs
@@ -6,17 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Enemy_Basic : MonoBehaviour
 {
+    public float offsetx=.55f;
     Rigidbody2D enemy;
     int move = 0;
-    const float movespeed = 1f;
     int direction = -1; //1 == right -1 == left
-    public int maxmove;
-    public GameObject player;
+    GameObject player;
+    Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindWithTag("Player");
         enemy = GetComponent<Rigidbody2D>();
-    }
+    } 
 
     // Update is called once per frame
     void Update()
@@ -27,19 +28,24 @@ public class Enemy_Basic : MonoBehaviour
 
     }
     //Movement
-    private void Move()
+    public void Move()
     {
+        //Change Offset depending on direction
+        if (direction == 1) { offset.x = offsetx; }
+        else { offset.x = -offsetx; }
         //Check for ground infront of object
         Vector2 _270 = -Vector2.up + (Vector2.right * direction);
-        var hit = Physics2D.Raycast(transform.position, _270, 10f);
+        var hit = Physics2D.Raycast(transform.position+offset, _270, 10f);
+        Debug.DrawRay(transform.position + offset, _270, Color.white);
         //If not change the direction
-        if (!hit || hit.collider.tag != "Ground")
+        if (!hit || hit.collider.gameObject.tag != "Ground")
         {
+            Debug.Log("SWITCH");
             direction *= -1;
             move = 0;
         }
         //Change direction if gets to max distance
-        if (move >= maxmove)
+        if (move >= 650)
         {
             direction *= -1;
             move = 0;
@@ -47,11 +53,11 @@ public class Enemy_Basic : MonoBehaviour
         //Increase Movement time
         move++;
         //Move the enemy
-        enemy.velocity = new Vector2(movespeed * direction, enemy.velocity.y);
+        enemy.velocity = new Vector2(direction, enemy.velocity.y);
     }
 
     //Move towards player if they are near
-    private void MoveTowardsPlayer()
+    public void MoveTowardsPlayer()
     {
         Debug.Log("MOVING TOWARDS PLAYER");
     }
