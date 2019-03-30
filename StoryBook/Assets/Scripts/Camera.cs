e232be830799d001c66ebfa996e93d3d96394682
Ryhime <ryhime100111@gmail.com>
2019-03-29 22:23:30 -0400

    Camera

---
+++ StoryBook/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0, 0, 0);
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(45, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = offset + player.transform.position;
+    }
+}
